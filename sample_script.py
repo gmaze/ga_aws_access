6e30eff1abe1d89a782e6ad8a974601f5850e11e
Guillaume Maze <gmaze@ifremer.fr>
2024-07-10 11:32:02 +0200

    Update sample_script.py

--- sample_script.py
+++ sample_script.py
@@ -4,16 +4,20 @@ from botocore.client import Config
 
 if __name__ == "__main__":
 
-    fs = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+    # fs = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+    fs = boto3.client('s3')
 
     # List the object:
+    print("\n", "="*40, "\n")
     object_list = fs.list_objects_v2(Bucket='argo-gdac-sandbox', Prefix="pub/idx/argo_synthetic-profile_index.txt.gz")
     print(object_list)
+    print("\n", "="*40, "\n")
 
     # Select data from the object:
+    print("\n", "="*40, "\n")
     s3_object = fs.select_object_content(
-                    Bucket='argotests',
-                    Key='ar_index_global_prof.txt.gz',
+                    Bucket='argo-gdac-sandbox',
+                    Key='pub/idx/argo_synthetic-profile_index.txt.gz',
                     ExpressionType="SQL",
                     Expression="SELECT COUNT(*) FROM s3object",
                     InputSerialization={"CSV": {"FileHeaderInfo": "IGNORE",
@@ -37,3 +41,4 @@ if __name__ == "__main__":
             stats = event['Stats']['Details']
 
     print(''.join(r for r in records))
+    print("\n", "="*40, "\n")
